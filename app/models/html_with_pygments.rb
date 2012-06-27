d787323724892500645c57017075791aff10d4f2
Shun Matsumoto <shun.matsumoto@pixta.co.jp>
2012-06-27 11:15:13 +0900

    update

--- app/models/html_with_pygments.rb
+++ app/models/html_with_pygments.rb
@@ -6,7 +6,7 @@ class HtmlWithPygments < Redcarpet::Render::HTML
   #------------#
   # コードハイライト
   def block_code( code, language )
-    RubyPython.configure :python_exe => 'python2.7'
+    RubyPython.configure :python_exe => 'python2.6'
     Pygments.highlight( code, lexer: language, options: { encoding: 'utf-8' } )
   end
 
